3d41e1725a5b90dfbe35e0a066915493ae64bb26
Eder Santos <edersnk@hotmail.com>
2026-04-15 09:41:47 -0300

    teste

--- main.go
+++ main.go
@@ -6,6 +6,6 @@ func main() {
 	fmt.Println("Desenvolvimento")
 	fmt.Println("Feature-XPTO")
 	fmt.Println("Release")
-	fmt.Println("Hotfix")
+	fmt.Println("Teste-Hotfix")
 	fmt.Println("Novo-Hotfix")
 }
