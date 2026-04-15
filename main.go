f7f18faf2277abde679b15e4a2d56292b86416d6
Eder Santos <edersnk@hotmail.com>
2026-04-15 09:36:52 -0300

    Novo hotfix

--- main.go
+++ main.go
@@ -2,9 +2,10 @@ package main
 
 import "fmt"
 
-func main(){
+func main() {
 	fmt.Println("Desenvolvimento")
 	fmt.Println("Feature-XPTO")
 	fmt.Println("Release")
+	fmt.Println("Hotfix")
 	fmt.Println("Hotfix")
 }
